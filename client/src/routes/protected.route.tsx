d4628dc09a85e0dfd819a1675b6abbde3783481c
Saurabh Tomar <83553327+SaurabhTomar123@users.noreply.github.com>
2025-09-09 16:26:22 +0530

    Update protected.route.tsx

--- client/src/routes/protected.route.tsx
+++ client/src/routes/protected.route.tsx
@@ -3,12 +3,19 @@ import useAuth from "@/hooks/api/use-auth";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
+  const disableAuth = import.meta.env.VITE_DISABLE_AUTH === "true";
+
+  if (disableAuth) {
+    return <Outlet />;
+  }
+
   const { data: authData, isLoading } = useAuth();
   const user = authData?.user;
 
   if (isLoading) {
     return <DashboardSkeleton />;
   }
+
   return user ? <Outlet /> : <Navigate to="/" replace />;
 };
 
